f5a00261b0036ab43268755ba86f134ab0986ac3
KONTROL Tecnologia <guzzy@uol.com.br>
2026-05-18 22:39:46 -0300

    Fix encoder direction/select and apply display offset+invert

--- src/MenuSystem.cpp
+++ src/MenuSystem.cpp
@@ -9,9 +9,9 @@ MenuSystem::MenuSystem(DisplayManager& display, SdManager& sd, BuzzerManager& bu
     : display_(display), sd_(sd), buzzer_(buzzer), monitor_(monitor), codecs_(codecs), codecCount_(codecCount) {}
 
 void MenuSystem::begin() {
-    pinMode(Pins::BTN_UP, INPUT_PULLUP);
-    pinMode(Pins::BTN_DOWN, INPUT_PULLUP);
-    pinMode(Pins::BTN_SELECT, INPUT_PULLUP);
+    pinMode(Pins::ENCODER_LEFT, INPUT_PULLUP);
+    pinMode(Pins::ENCODER_RIGHT, INPUT_PULLUP);
+    pinMode(Pins::ENCODER_PUSH, INPUT_PULLUP);
     pinMode(Pins::BTN_BACK, INPUT_PULLUP);
     render();
 }
@@ -38,14 +38,28 @@ void MenuSystem::update() {
 }
 
 MenuSystem::Button MenuSystem::readButton() {
+    static uint8_t lastClkState = HIGH;
     if (millis() - lastButtonMs_ < Config::MENU_DEBOUNCE_MS) return Button::None;
-    Button pressed = Button::None;
-    if (digitalRead(Pins::BTN_UP) == LOW) pressed = Button::Up;
-    else if (digitalRead(Pins::BTN_DOWN) == LOW) pressed = Button::Down;
-    else if (digitalRead(Pins::BTN_SELECT) == LOW) pressed = Button::Select;
-    else if (digitalRead(Pins::BTN_BACK) == LOW) pressed = Button::Back;
-    if (pressed != Button::None) lastButtonMs_ = millis();
-    return pressed;
+
+    if (digitalRead(Pins::ENCODER_PUSH) == LOW) {
+        lastButtonMs_ = millis();
+        return Button::Select;
+    }
+    if (digitalRead(Pins::BTN_BACK) == LOW) {
+        lastButtonMs_ = millis();
+        return Button::Back;
+    }
+
+    const uint8_t clkState = digitalRead(Pins::ENCODER_LEFT);
+    const uint8_t dtState = digitalRead(Pins::ENCODER_RIGHT);
+    if (lastClkState == HIGH && clkState == LOW) {
+        lastButtonMs_ = millis();
+        lastClkState = clkState;
+        return (dtState == HIGH) ? Button::Down : Button::Up;
+    }
+
+    lastClkState = clkState;
+    return Button::None;
 }
 
 void MenuSystem::move(int delta, uint8_t count) {
