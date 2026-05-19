c7e4085bd6f7f64422b38dc6a1cef4fc90570326
KONTROL Tecnologia <guzzy@uol.com.br>
2026-05-18 23:34:59 -0300

    Skip boot self-tests and enter menu directly

--- src/main.cpp
+++ src/main.cpp
@@ -22,38 +22,26 @@ MSXCodec msx;
 PlatformCodec* codecs[] = {&zxSpectrum, &zx81, &msx};
 MenuSystem menu(display, sd, buzzer, inputMonitor, codecs, sizeof(codecs) / sizeof(codecs[0]));
 
-static void runSelfTest() {
+static void initSystem() {
     Serial.begin(Config::SERIAL_BAUD);
-    delay(250);
-    Serial.println("Digital Datasette boot");
 
-    const bool displayOk = display.begin();
-    display.splash();
-    delay(450);
-    // Orientation/color test removed to avoid boot-stop reports on this screen.
-
+    display.begin();
     inputMonitor.begin();
-    const bool adcOk = true;
 
     pinMode(Pins::DIGITAL_IN_PIN, INPUT);
     audioOutput.begin();
     buzzer.begin();
-    buzzer.beep(1800, 70);
 
-    const bool sdOk = sd.begin();
-    if (sdOk) {
+    if (sd.begin()) {
         bool savedBuzzer = buzzer.isEnabled();
         if (sd.loadBuzzerEnabled(savedBuzzer)) buzzer.setEnabled(savedBuzzer);
         sd.ensurePlatformFolders();
     }
-
-    display.showSelfTest(displayOk, adcOk, sdOk, buzzer.isEnabled());
-    delay(1100);
 }
 
 #ifndef UNIT_TEST
 void setup() {
-    runSelfTest();
+    initSystem();
     menu.begin();
 }
 
@@ -62,7 +50,7 @@ void loop() {
 }
 #else
 int main() {
-    runSelfTest();
+    initSystem();
     menu.begin();
     for (int i = 0; i < 2; ++i) menu.update();
     return 0;
